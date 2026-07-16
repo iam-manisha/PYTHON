b620e1aba825b1f2a6bec37f34778c37824dfe19
Manisha Sahu <sahumanisha.git06@gmail.com>
2026-07-16 23:57:58 +0530

    ok

--- Basics/halorect.py
+++ Basics/halorect.py
@@ -1,3 +1,8 @@
 num = 4
-for row in range (1,num *2 +1):
+for row in range (1,num + 1):
     for col in range (1,num *2 +1):
+        if row == 1 or col == 1 or row == num or col == num*2:
+            print("*",end='  ' )
+        else:
+            print(' ',end = '  ')
+    print()
